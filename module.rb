d065283c1fa25fe8e6d758ac564a411cbaa30d12
myelin0 <emi.veisllari@gmail.com>
2022-07-22 10:49:51 +0200

    Add Class and module.rb

---
+++ module.rb
@@ -0,0 +1,14 @@
+module MyEnumerable
+  def all?(&block)
+    compute = true
+    each { |i| compute = false unless block.yield(i) }
+    compute
+  end
+
+  def any?(&block)
+    compute = false
+    each { |i| compute = true if block.yield(i) }
+    compute
+  end
+
+end
